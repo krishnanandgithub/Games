8c912eb29c98e3b1b7e0b64d5986d43b525b1441
Krishnanand Mishra <110751379+krishnanandgithub@users.noreply.github.com>
2024-11-21 11:54:13 +0530

    Update rolling_dice.js

--- rolling_dice.js
+++ rolling_dice.js
@@ -5,7 +5,7 @@ function getDiceRoll() {
 function diceFor1() {
   const row1 = '┏━━━━━━━━┓\n';
   const row2 = '┃        ┃\n';
-  const row3 = '┃   ⚪   ┃\n'
+  const row3 = '┃   ⚪   ┃\n';
   const row4 = '┃        ┃\n';
   const row5 = '┗━━━━━━━━┛\n';
 
@@ -15,7 +15,7 @@ function diceFor1() {
 function diceFor2() {
   const row1 = '┏━━━━━━━━┓\n';
   const row2 = '┃ ⚪     ┃\n';
-  const row3 = '┃        ┃\n'
+  const row3 = '┃        ┃\n';
   const row4 = '┃     ⚪ ┃\n';
   const row5 = '┗━━━━━━━━┛\n';
 
@@ -25,7 +25,7 @@ function diceFor2() {
 function diceFor3() {
   const row1 = '┏━━━━━━━━┓\n';
   const row2 = '┃ ⚪     ┃\n';
-  const row3 = '┃   ⚪   ┃\n'
+  const row3 = '┃   ⚪   ┃\n';
   const row4 = '┃     ⚪ ┃\n';
   const row5 = '┗━━━━━━━━┛\n';
 
@@ -35,7 +35,7 @@ function diceFor3() {
 function diceFor4() {
   const row1 = '┏━━━━━━━━┓\n';
   const row2 = '┃ ⚪  ⚪ ┃\n';
-  const row3 = '┃        ┃\n'
+  const row3 = '┃        ┃\n';
   const row4 = '┃ ⚪  ⚪ ┃\n';
   const row5 = '┗━━━━━━━━┛\n';
 
@@ -45,7 +45,7 @@ function diceFor4() {
 function diceFor5() {
   const row1 = '┏━━━━━━━━┓\n';
   const row2 = '┃ ⚪  ⚪ ┃\n';
-  const row3 = '┃   ⚪   ┃\n'
+  const row3 = '┃   ⚪   ┃\n';
   const row4 = '┃ ⚪  ⚪ ┃\n';
   const row5 = '┗━━━━━━━━┛\n';
 
@@ -55,11 +55,23 @@ function diceFor5() {
 function diceFor6() {
   const row1 = '┏━━━━━━━━┓\n';
   const row2 = '┃ ⚪  ⚪ ┃\n';
-  const row3 = '┃ ⚪  ⚪ ┃\n'
+  const row3 = '┃ ⚪  ⚪ ┃\n';
   const row4 = '┃ ⚪  ⚪ ┃\n';
   const row5 = '┗━━━━━━━━┛\n';
 
   return row1 + row2 + row3 + row4 + row5;
 }
 
-console.log(diceFor6());
+function diceRolledOutput() {
+  prompt('Press Return to roll the dice: ')
+  switch (getDiceRoll()) {
+    case 1: return diceFor1();
+    case 2: return diceFor2();
+    case 3: return diceFor3();
+    case 4: return diceFor4(); 
+    case 5: return diceFor5();
+    case 6: return diceFor6();
+  }
+}
+
+console.log(diceRolledOutput());
